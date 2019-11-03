3796d36cd5e2c8d0381f75ec133c401b5aae87c2
pikafan123 <lerayplayzz@gmail.com>
2019-11-03 13:09:46 +0100

    Upload Code

---
+++ Brainpreter/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Brainpreter
+{
+    enum Token
+    {
+        ShiftRight,
+        ShiftLeft,
+        Plus,
+        Minus,
+        Output,
+        Input,
+        StartLoop,
+        EndLoop
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            if (input.Replace("[", "").Length == input.Replace("]", "").Length)
+            {
+            var tokens = TokenizeString(input).ToArray();
+            int pointer = 0;
+            long regSize = 34359738352;
+            byte[] register = new byte[regSize];
+            int looppointer = 0;
+            for(int i = 0; i < tokens.Length; i++)
+            {
+                Token token = tokens[i];
+                switch(token)
+                {
+                    case Token.ShiftLeft:
+                        if (pointer == 0)
+                        {
+                            System.Console.WriteLine("Error");
+                            throw new InvalidOperationException();
+                        }
+                        pointer--;
+                        break;
+                    case Token.ShiftRight:
+                        if (pointer == regSize)
+                        {
+                            System.Console.WriteLine("Error");
+                            throw new InvalidOperationException();
+                        }
+                        pointer++;
+                        break;
+                    case Token.Plus:
+                        register[pointer]++;
+                        break;
+                    case Token.Minus:
+                        register[pointer]--;
+                        break;
+                    case Token.Output:
+                        Console.Write((char)register[pointer]);
+                        break;
+                    case Token.Input:
+                        register[pointer] = Convert.ToByte(Console.ReadKey().KeyChar);
+                        break;
+                    case Token.StartLoop:
+                        if (register[pointer] == 0)
+                        {
+                            i++;
+                            while (looppointer > 0 || tokens[i] != Token.EndLoop)
+                            {
+                                if (tokens[i] == Token.StartLoop)
+                                {
+                                    looppointer++;
+                                }
+                                if (tokens[i] == Token.EndLoop)
+                                {
+                                    looppointer--;
+                                }
+                                i++;
+                            }
+                        }
+                        break;
+                    case Token.EndLoop:
+                        i--;
+                        while (looppointer > 0 || tokens[i] != Token.StartLoop)
+                        {
+                            if (tokens[i] == Token.EndLoop)
+                            {
+                                looppointer++;
+                            }
+                            if (tokens[i] == Token.StartLoop)
+                            {
+                                looppointer--;
+                            }
+                            i--;
+                        }
+                        i--;
+                        break;
+                }
+            }
+            }
+            else
+            {
+                System.Console.WriteLine("Error");
+            }
+        }
+        static List<Token> TokenizeString(string input)
+        {
+            List<Token> Tokens = new List<Token>();
+            foreach (char c in input)
+            {
+                switch(c)
+                {
+                    case '>':
+                        Tokens.Add(Token.ShiftRight);
+                        break;
+                    case '<':
+                        Tokens.Add(Token.ShiftLeft);
+                        break;
+                    case '+':
+                        Tokens.Add(Token.Plus);
+                        break;
+                    case '-':
+                        Tokens.Add(Token.Minus);
+                        break;
+                    case '.':
+                        Tokens.Add(Token.Output);
+                        break;
+                    case ',':
+                        Tokens.Add(Token.Input);
+                        break;
+                    case '[':
+                        Tokens.Add(Token.StartLoop);
+                        break;
+                    case ']':
+                        Tokens.Add(Token.EndLoop);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return Tokens;
+        }
+    }
+}
---
+++ Brainverter/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Math;
+namespace Brainverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input;
+            if (args.Length != 0)
+            {
+                var inp = "";
+                args.ToList().ForEach(x => inp += (x + " "));
+                inp = inp.Substring(0, inp.Length - 1);
+                input = inp.ToCharArray();
+            }
+            else
+            {
+                input = Console.ReadLine().ToCharArray();
+            }
+            List<int> Linted = new List<int>();
+            input.ToList().ForEach(x => Linted.Add(Convert.ToInt32(x)));
+
+            System.Console.WriteLine(ConvertToShortFudge(Linted.ToArray()));
+        }
+        // Old Code
+        static string ConvertToFudge(int[] ints)
+        {
+            string fudge = "";
+            foreach(int intl in ints)
+            {
+                for (int i = 0; i < intl; i++)
+                {
+                    fudge += "+";
+                }
+                fudge += ".>";
+            }
+            return fudge;
+        }
+        static string ConvertToShortFudge(int[] ints)
+        {
+            string fudge = "";
+            foreach (int intl in ints)
+            {
+                // This is just a variable to control which Divider returns a short program
+                int Divider = 8;
+                int multOfDiv = DivRem(intl, Divider, out int rem);
+                string mdivPlus = "";
+                string remPlus = "";
+                string divPlus = "";
+                for (int i = 0; i < multOfDiv; i++)
+                    mdivPlus += "+";
+                for (int i = 0; i < rem; i++)
+                    remPlus += "+";
+                for (int i = 0; i < Divider; i++)
+                    divPlus += "+";
+                fudge += $">{mdivPlus}[<{divPlus}>-]<{remPlus}.>";
+            }
+            return fudge;
+        }
+    }
+}
